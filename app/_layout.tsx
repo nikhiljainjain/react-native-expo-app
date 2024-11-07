70f05ff34b5b38760a20c07b1244671fc2925f25
Nikhil Jain <nikhil.jain@cityexperiences.com>
2024-11-07 19:12:42 +0530

    Android build

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,14 @@
 import { Stack } from 'expo-router';
+import { setStatusBarStyle } from 'expo-status-bar';
+import { useEffect } from 'react';
 
 export default function RootLayout() {
+    useEffect(() => {
+        setTimeout(() => {
+            setStatusBarStyle('light');
+        });
+    });
+
     return (
         <Stack>
             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
